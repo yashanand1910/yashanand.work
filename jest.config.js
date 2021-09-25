d8ff4dc471ec105a6710ec39cfcbdcc24ac12d71
Yash Anand <yashanand1910@gmail.com>
2021-09-25 08:48:47 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  coverageDirectory: 'reports',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  globals: {
+    'ts-jest': {
+      allowSyntheticDefaultImports: true,
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      diagnostics: {
+        ignoreCodes: ['TS151001'],
+      }
+    },
+  },
+  // Do not ignore librairies such as ionic, ionic-native or bootstrap to transform them during unit testing.
+  transformIgnorePatterns: ['node_modules/(?!(jest-test|@ng-bootstrap))']
+};
