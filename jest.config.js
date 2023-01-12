aeb4a87a4e886e334b48786a90e7616223f489dc
Yash Anand <yashanand1910@gmail.com>
2023-01-12 18:56:02 -0500

    chore: some cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 // https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
+
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: ['src'],
+  roots: ['<rootDir>/src'],
   coverageDirectory: 'reports',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   globals: {
     'ts-jest': {
       allowSyntheticDefaultImports: true,
