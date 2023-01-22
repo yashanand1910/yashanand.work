2c725c35bb4a0a4a84432222ebfb68a1dfae678d
Yash Anand <yashanand1910@gmail.com>
2023-01-21 20:04:21 -0500

    ci(environment): update secret variables

--- src/app/shared/http/authorization.interceptor.ts
+++ src/app/shared/http/authorization.interceptor.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '@env/environment';
 
+/**
+ * Add Notion authorization header (see https://developers.notion.com/docs/authorization)
+ */
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request.headers.set('Authorization', `Bearer ${environment.notion.secret}`);
     return next.handle(request);
   }
 }
