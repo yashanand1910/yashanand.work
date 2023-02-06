90e091375c794c77fb9d711cb83f05838087e8b0
Yash Anand <yashanand1910@gmail.com>
2023-02-06 01:53:20 -0500

    feat(BlogService): implement getPosts()

--- src/app/blog/blog.service.ts
+++ src/app/blog/blog.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@angular/core';
 import { Post } from '@app/model/blog';
 import { Logger } from '@app/shared';
 import { environment } from '@env/environment';
-import { map } from 'rxjs';
+import {
+  PageObjectResponse,
+  PartialPageObjectResponse,
+  QueryDatabaseParameters,
+  QueryDatabaseResponse
+} from '@notionhq/client/build/src/api-endpoints';
+import { map, Observable } from 'rxjs';
 
 const log = new Logger('BlogService');
 
@@ -11,15 +17,43 @@ const log = new Logger('BlogService');
   providedIn: 'root'
 })
 export class BlogService {
-  private databaseId = environment.notion.databaseId;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  private databaseId = environment.notion.databaseId!;
 
   constructor(private http: HttpClient) {}
 
-  getPosts() {
-    const query = {}; // empty query for all posts
+  private queryDatabase = (id: string, query: Partial<QueryDatabaseParameters>) => {
+    return this.http.post<QueryDatabaseResponse>(`/databases/${id}/query`, JSON.stringify(query));
+  };
 
-    return this.http
-      .post<Post[]>(`/databases/${this.databaseId}/query`, JSON.stringify(query))
-      .pipe(map((res) => log.debug(res)));
-  }
+  getPosts = () => {
+    return this.queryDatabase(this.databaseId, {}).pipe(
+      map((response: QueryDatabaseResponse) => {
+        return response.results.map<Post>((res: PageObjectResponse | PartialPageObjectResponse) => {
+          const page = <PageObjectResponse>res;
+          log.debug(page);
+          return {
+            id: page.id,
+            author: this.getUserName(page, 'Created by'),
+            title: this.getText(page, 'Title'),
+            created: new Date(page.created_time),
+            edited: new Date(page.last_edited_time),
+            editedBy: this.getUserName(page, 'Last edited by')
+          };
+        });
+      })
+    );
+  };
+
+  /***************
+   *  UTILITIES  *
+   ***************/
+
+  private getText = (page: PageObjectResponse, name: string) => {
+    return page.properties[name][name.toLowerCase()][0]['plain_text'];
+  };
+
+  private getUserName = (page: PageObjectResponse, name: string) => {
+    return page.properties[name][name.toLowerCase().split(' ').join('_')]['name'];
+  };
 }
