c0505f4cea7fba1cd2c6659ba66b6e9f1e1f820f
Yash Anand <yashanand1910@gmail.com>
2024-11-20 10:30:55 -0500

    feat(footer): add public key

--- src/app/shell/footer/footer.component.ts
+++ src/app/shell/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   currentYear = new Date().getFullYear();
+  publicKeyLink = 'https://key.yashanand.dev';
   socialLinks = [
     {
       name: 'email',
